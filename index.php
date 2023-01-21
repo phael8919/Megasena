151f625bc31d452d429297c375b37ead4badf8c4
unknown <raphaelphlnovo@gmail.com>
2023-01-21 11:08:48 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="Container">
+        <div>
+            <h2>Gerar Números da Mega Sena</h2>
+        </div>
+        <div>
+            <button>Clique aqui</button>
+        </div>
+        <div>
+            <?php 
+                
+            
+            ?>
+        </div>
+    </div>
+</body>
+</html>
